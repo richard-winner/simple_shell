f8a9659e31ff8909d5fcb47a5333371d195d5498
dr winner <duvorrichardwinner@gmail.com>
2023-04-26 15:53:41 -0700

    updated

--- environ.c
+++ environ.c
@@ -8,8 +8,8 @@
  */
 int _myenv(info_t *info)
 {
-	print_list_str(info->env);
-	return (0);
+print_list_str(info->env);
+return (0);
 }
 
 /**
@@ -21,17 +21,16 @@ int _myenv(info_t *info)
  */
 char *_getenv(info_t *info, const char *name)
 {
-	list_t *node = info->env;
-	char *p;
-
-	while (node)
-	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
-	}
-	return (NULL);
+list_t *node = info->env;
+char *p;
+while (node)
+{
+p = starts_with(node->str, name);
+if (p && *p)
+return (p);
+node = node->next;
+}
+return (NULL);
 }
 
 /**
@@ -43,35 +42,33 @@ char *_getenv(info_t *info, const char *name)
  */
 int _mysetenv(info_t *info)
 {
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguements\n");
+return (1);
+}
+if (_setenv(info, info->argv[1], info->argv[2]))
+return (0);
+return (1);
 }
 
 /**
  * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * Return: Always 0
+ *  Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
-	int i;
-
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
+int i;
+if (info->argc == 1)
+{
+_eputs("Too few arguements.\n");
+return (1);
+}
+for (i = 1; i <= info->argc; i++)
+_unsetenv(info, info->argv[i]);
+return (0);
 }
 
 /**
@@ -82,12 +79,10 @@ int _myunsetenv(info_t *info)
  */
 int populate_env_list(info_t *info)
 {
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
+list_t *node = NULL;
+size_t i;
+for (i = 0; environ[i]; i++)
+add_node_end(&node, environ[i], 0);
+info->env = node;
+return (0);
 }
-
