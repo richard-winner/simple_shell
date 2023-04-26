c0e8f7ecb8578c04939f4bb9e2191a99cdbfa964
dr winner <duvorrichardwinner@gmail.com>
2023-04-26 13:47:20 -0700

    updated

--- _atoi.c
+++ _atoi.c
@@ -1,51 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "shell.h"
 
-int main(void)
+/**
+ * interactive - check if the shell is in interactive mode
+ * @info: struct containing shell information
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char *args[2];
-    int status;
-    pid_t pid;
+	return (isatty(STDIN_FILENO) && info->readfd <= STDERR_FILENO);
+}
 
-    while (1) {
-        printf("$ ");
-        nread = getline(&line, &len, stdin);
-        if (nread == -1) {
-            if (feof(stdin)) {
-                exit(EXIT_SUCCESS);
-            } else {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
-        }
-        if (line[nread-1] == '\n') {
-            line[nread-1] = '\0';
-        }
-        args[0] = line;
-        args[1] = NULL;
-        pid = fork();
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            /* Child process */
-            if (execve(args[0], args, NULL) == -1) {
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            /* Parent process */
-            if (wait(&status) == -1) {
-                perror("wait");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
-    return 0;
+/**
+ * is_delim - checks if character is a delimiter
+ * @c: the character to check
+ * @delim: the delimiter string
+ *
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim != '\0') {
+		if (*delim == c)
+			return (1);
+		delim++;
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: the character to check
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the converted number, or 0 if no numbers in string
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++) {
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9') {
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		} else if (flag == 1) {
+			flag = 2;
+		}
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
